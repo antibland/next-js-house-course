fbf5573e48b02e4f1067fe5e065c3dcee963b042
Andy Hoffman <antibland@gmail.com>
2021-02-14 11:07:50 -0800

    feat: set up basic navigation & layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
-import { AppProps } from "next/app";
-// import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+import "../styles/index.css";
+
+import { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <>
+    <Head>
+      <title>Home Sweet Home</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Component {...pageProps} /></>;
 }
