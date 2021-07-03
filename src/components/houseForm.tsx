453c5917026e2dd12b1c436c72e3c9af7048b12a
Andy Hoffman <antibland@gmail.com>
2021-07-03 01:11:52 -0700

    feat: Build search combo box and fetch results from Google

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -4,7 +4,7 @@ import { useForm } from "react-hook-form";
 // import { useRouter } from "next/router";
 // import Link from "next/link";
 // import { Image } from "cloudinary-react";
-// import { SearchBox } from "./searchBox";
+import { SearchBox } from "./searchBox";
 // import {
 //   CreateHouseMutation,
 //   CreateHouseMutationVariables,
@@ -31,6 +31,8 @@ export default function HouseForm({}: IProps) {
     IFormData
   >({ defaultValues: {} });
 
+  const address = watch("address");
+
   useEffect(() => {
     register({ name: "address" }, { required: "Please enter your address" });
     register({ name: "latitude" }, { required: true, min: -90, max: 90 });
@@ -52,8 +54,16 @@ export default function HouseForm({}: IProps) {
         <label htmlFor="search" className="block">
           Search for your address
         </label>
-        {/* SEARCH FIELD  */}
+        <SearchBox
+          onSelectAddress={(address, latitude, longitude) => {
+            setValue("address", address);
+            setValue("latitude", latitude);
+            setValue("longitude", longitude);
+          }}
+          defaultValue=""
+        />
         {errors.address && <p>{errors.address.message}</p>}
+        <h2>{address}</h2>
       </div>
     </form>
   );
