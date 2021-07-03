a75261c2135fcfa42b8f4887696ef31c8334a2fb
Andy Hoffman <antibland@gmail.com>
2021-07-03 11:51:34 -0700

    feat: Add additional house fields

--- src/components/houseForm.tsx
+++ src/components/houseForm.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect, ChangeEvent } from "react";
 import { useForm } from "react-hook-form";
 // import { useMutation, gql } from "@apollo/client";
 // import { useRouter } from "next/router";
-// import Link from "next/link";
+import Link from "next/link";
 // import { Image } from "cloudinary-react";
 import { SearchBox } from "./searchBox";
 // import {
@@ -26,7 +26,8 @@ interface IFormData {
 interface IProps {}
 
 export default function HouseForm({}: IProps) {
-  const [submitting, setSubmitting] = useState<Boolean>(false);
+  const [previewImage, setPreviewImage] = useState<string>();
+  const [submitting, setSubmitting] = useState(false);
   const { register, handleSubmit, setValue, errors, watch } = useForm<
     IFormData
   >({ defaultValues: {} });
@@ -63,8 +64,86 @@ export default function HouseForm({}: IProps) {
           defaultValue=""
         />
         {errors.address && <p>{errors.address.message}</p>}
-        <h2>{address}</h2>
       </div>
+
+      {address && (
+        <>
+          <div className="mt-4">
+            <label
+              htmlFor="image"
+              className="p-4 border-dashed border-4 border-gray-600 block cursor-pointer"
+            >
+              Click to add image 16:9
+            </label>
+            <input
+              type="file"
+              name="image"
+              id="image"
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={(event: ChangeEvent<HTMLInputElement>) => {
+                if (event?.target?.files?.[0]) {
+                  const file = event.target.files[0];
+                  const reader = new FileReader();
+                  reader.onloadend = () => {
+                    setPreviewImage(reader.result as string);
+                  };
+                  reader.readAsDataURL(file);
+                }
+              }}
+              ref={register({
+                validate: (filelist: FileList) => {
+                  if (filelist.length === 1) return true;
+                  return "Please upload one file";
+                },
+              })}
+            />
+            {previewImage && (
+              <img
+                src={previewImage}
+                alt=""
+                className="mt-4 object-cover"
+                style={{ width: "576px", height: `${(9 / 16) * 576}px` }}
+              />
+            )}
+            {errors.image && <p>{errors.image.message}</p>}
+          </div>
+
+          <div className="mt-4">
+            <label htmlFor="bedrooms" className="block">
+              Beds
+            </label>
+            <input
+              type="number"
+              name="bedrooms"
+              id="bedrooms"
+              className="p-2"
+              ref={register({
+                required: "Please enter the number of bedrooms",
+                max: { value: 10, message: "That's too many bedrooms—sorry" },
+                min: {
+                  value: 1,
+                  message: "That's too thin in the bedroom department",
+                },
+              })}
+            />
+            {errors.bedrooms && <p>{errors.bedrooms.message}</p>}
+          </div>
+
+          <div className="mt-4">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 font-bold py-2 px-4 rounded"
+              type="submit"
+              disabled={submitting}
+            >
+              Save
+            </button>{" "}
+            <Link href="/">
+              <a className="ml-2">Cancel</a>
+            </Link>
+          </div>
+        </>
+      )}
     </form>
   );
 }
