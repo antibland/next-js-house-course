432248f9b1d033c19607cd19c3f30f84db7b8733
Andy Hoffman <antibland@gmail.com>
2021-07-05 21:03:39 -0700

    feat: Add single map to map detail page

--- pages/houses/[id]/index.tsx
+++ pages/houses/[id]/index.tsx
@@ -3,7 +3,7 @@ import { Image } from "cloudinary-react";
 import { useQuery, gql } from "@apollo/client";
 import Layout from "src/components/layout";
 // import HouseNav from "src/components/houseNav";
-// import SingleMap from "src/components/singleMap";
+import SingleMap from "src/components/singleMap";
 import {
   ShowHouseQuery,
   ShowHouseQueryVariables,
@@ -65,7 +65,9 @@ function HouseData({ id }: { id: string }) {
             />
             <p>{house.bedrooms} 🛏 house</p>
           </div>
-          <div className="sm:w-full md:w-1/2"></div>
+          <div className="sm:w-full md:w-1/2">
+            <SingleMap house={house} />
+          </div>
         </div>
       }
     />
