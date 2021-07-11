91e7b698ddaa76f541fa65716694b391974e491b
Andy Hoffman <antibland@gmail.com>
2021-07-10 20:58:46 -0700

    feat: add delete mutation

--- src/components/houseNav.tsx
+++ src/components/houseNav.tsx
@@ -2,7 +2,13 @@ import { useMutation, gql } from "@apollo/client";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import { useAuth } from "src/auth/useAuth";
-// import { DeleteHouse, DeleteHouseVariables } from "src/generated/DeleteHouse";
+import { DeleteHouse, DeleteHouseVariables } from "src/generated/DeleteHouse";
+
+const DELETE_HOUSE_MUTATION = gql`
+  mutation DeleteHouse($id: String!) {
+    deleteHouse(id: $id)
+  }
+`;
 
 interface IProps {
   house: {
@@ -12,8 +18,14 @@ interface IProps {
 }
 
 export default function HouseNav({ house }: IProps) {
+  const router = useRouter();
   const { user } = useAuth();
   const canManage = !!user && user.uid === house.userId;
+  const [deleteHouse, { loading }] = useMutation<
+    DeleteHouse,
+    DeleteHouseVariables
+  >(DELETE_HOUSE_MUTATION);
+
   return (
     <>
       <Link href="/">
@@ -25,6 +37,19 @@ export default function HouseNav({ house }: IProps) {
           <Link href={`/houses/${house.id}/edit`}>
             <a>edit</a>
           </Link>
+          {" | "}
+          <button
+            disabled={loading}
+            type="button"
+            onClick={async () => {
+              if (confirm("Are you sure?")) {
+                await deleteHouse({ variables: { id: house.id } });
+                router.push("/");
+              }
+            }}
+          >
+            delete
+          </button>
         </>
       )}
     </>
